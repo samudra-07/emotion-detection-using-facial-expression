48d1d0c706683851848f00bcc86277b5494b96b0
Samudragupta Barma <samudraguptabarma@gmail.com>
2025-04-30 23:41:38 +0530

    new update

--- realTime.py
+++ realTime.py
@@ -2,34 +2,37 @@ import cv2
 from keras.models import model_from_json
 import numpy as np
 
+# Load the model
 json_file = open("emotionRecognition.json", "r")
 model_json = json_file.read()
 json_file.close()
 model = model_from_json(model_json)
 model.load_weights("emotionRecognition.keras")
+
+# Load the Haar cascade
 haar_file = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
 face_cascade = cv2.CascadeClassifier(haar_file)
 
+# Feature extraction function
 def extract_features(image):
     feature = np.array(image)
     feature = feature.reshape(1, 48, 48, 1)
     return feature / 255.0
 
+# Initialize webcam
 webcam = cv2.VideoCapture(0)
 labels = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
 
-screen_width = 1366
-screen_height = 768
-
+# Create a resizable window
 cv2.namedWindow("Output", cv2.WINDOW_NORMAL)
-cv2.setWindowProperty("Output", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
 while True:
-    i, im = webcam.read()
+    ret, im = webcam.read()
+    if not ret:
+        break
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(im, 1.3, 5)
-    
-    try: 
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    try:
         for (p, q, r, s) in faces:
             image = gray[q:q+s, p:p+r]
             cv2.rectangle(im, (p, q), (p+r, q+s), (255, 0, 0), 2)
@@ -37,16 +40,14 @@ while True:
             img = extract_features(image)
             pred = model.predict(img)
             prediction_label = labels[pred.argmax()]
-            cv2.putText(im, '% s' %(prediction_label), (p-10, q-10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 0, 255))
-        
-        im = cv2.resize(im, (screen_width, screen_height))
-        
+            cv2.putText(im, '%s' % (prediction_label), (p-10, q-10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 0, 255))
         cv2.imshow("Output", im)
-
-        if cv2.waitKey(1) == 27:
-            break
     except cv2.error:
         pass
 
+    if cv2.waitKey(27) & 0xFF == 27:  # Exit on pressing 'Esc'
+        break
+
+# Release the webcam and close windows
 webcam.release()
 cv2.destroyAllWindows()
